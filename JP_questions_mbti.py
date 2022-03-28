f8905bee5aff87c90a976d0539c857d37c1b5e06
aderepas <49250442+aderepas@users.noreply.github.com>
2022-03-28 19:09:06 +0200

    Add files via upload

---
+++ JP_questions_mbti.py
@@ -0,0 +1,170 @@
+question_about_mbti = {
+	'T' : {
+		'1' : {
+			'Ti' : "新しいプロジェクトを開始したいと考えています。 設計者のように、最善のアプローチを見つけるためにあらゆる可能性を探ることで、ソリューションを構築します。atarashii purojekuto wo kaishi shi tai to kangae te i masu. sekkei sha no yō ni, saizen no apurōchi wo mitsukeru tame ni arayuru kanō sei wo saguru koto de, soryūshon wo kōchiku shi masu.",
+			'Te' : "プロジェクトを終了したいと考えています。 100%で最適なソリューションを見つけるよりも、完了することをお勧めします。 危機の場合には、あなたは、チームを終了するためにプッシュする指揮者です。purojekuto wo shūryō shi tai to kangae te i masu. 100%de saiteki na soryūshon wo mitsukeru yori mo, kanryō suru koto wo o susume shi masu. kiki no baai ni wa, anata wa, chīmu wo shūryō suru tame ni pusshu suru shiki sha desu." 
+		},
+		'2' : {
+			'Ti' : "アイデアを後で開発するために、アイデアを並べ替えて、その仕組みを理解したいと考えています。aidea wo atode kaihatsu suru tame ni, aidea wo narabe kae te, sono shikumi wo rikai shi tai to kangae te i masu.",
+			'Te' : "スケジューリング、定量化、および危機管理計画に適しています。sukejūringu, teiryō ka, oyobi kiki kanri keikaku ni tekishi te i masu." 
+		},
+		'3' : {
+			'Ti' : "物事がどのように異なるかを特定し、論理的にランク付けしたいと考えています。monogoto ga dono yō ni kotonaru ka wo tokutei shi, ronri teki ni ranku zuke shi tai to kangae te i masu.",
+			'Te' : "コントロールが必要であり、自分に影響を与えようとする人が好きではない。kontorōru ga hitsuyō de ari, jibun ni eikyō wo ataeyo u to suru hito ga suki de wa nai." 
+		},
+		'4' : {
+			'Ti' : "あなたは自分自身のための知識に情熱を注いでいます。 正確性は重要であり、アイデアが最も良いものであればどこからでも重要です。anata wa jibun jishin no tame no chishiki ni jōnetsu wo sosoi de i masu. seikaku sei wa jūyō de ari, aidea ga mottomo yoi mono de are ba doko kara demo jūyō desu.",
+			'Te' : "最もよい考えは有効、すぐに有用である。 情報は客観的で、秩序があり、合理的である必要があります。mottomo yoi kangae wa yūkō, sugu ni yūyō de aru. jōhō wa kyakkan teki de, chitsujo ga ari, gōri teki de aru hitsuyō ga ari masu." 
+		},
+		'5' : {
+			'Ti' : "皆さんは、私たちの結論に至るまでのすべてのステップに従うことに興奮しています。 つまり、純粋な事実よりも重要な意味を持ち、背後で何が起きているのかを理解したいと考えています。minasan wa, watashi tachi no ketsuron ni itaru made no subete no suteppu ni shitagau koto ni kōfun shi te i masu. tsumari, junsui na jijitsu yori mo jūyō na imi wo mochi, haigo de nani ga oki te iru no ka wo rikai shi tai to kangae te i masu.",
+			'Te' : "効果的に機能するものが欲しいだけです。 最終的な結果については、それにつながった反省のすべての段階とプロセスを理解するよりも、より関心があります。 事実は完璧であり、より広範な結論を出す必要はありません。kōka teki ni kinō suru mono ga hoshii dake desu. saishū teki na kekka nitsuite wa, sore ni tsunagatta hansei no subete no dankai to purosesu wo rikai suru yori mo, yori kanshin ga ari masu. jijitsu wa kanpeki de ari, yori kōhan na ketsuron wo dasu hitsuyō wa ari mase n." 
+		},
+		'6' : {
+			'Ti' : "概念を理解することは、そのアプリケーションよりも重要です。gainen wo rikai suru koto wa, sono apurikēshon yori mo jūyō desu.",
+			'Te' : "ソリューションの有効性は、問題の発生よりも重要です。soryūshon no yūkō sei wa, mondai no hassei yori mo jūyō desu." 
+		},
+		'7' : {
+			'Ti' : "あなたは本質的に推論的です。 理論を作成してから、事実に移ります。 あなたは、あなたにとって意味のあることに基づいて仮定を行っています。anata wa honshitsu teki ni suiron teki desu. riron wo sakusei shi te kara, jijitsu ni utsuri masu. anata wa, anata nitotte imi no aru koto ni motozui te katei wo otte i masu.",
+			'Te' : "あなたは本質的に誘導性です。 事実とは何かを決定し、それに基づいて理論を開発します。 あなたは、外部から提示された客観的な事実に基づいて決定を行います。anata wa honshitsu teki ni yūdō sei desu. jijitsu to wa nani ka wo kettei shi, sore ni motozui te riron wo kaihatsu shi masu. anata wa, gaibu kara teiji sa re ta kyakkan teki na jijitsu ni motozui te kettei wo okonai masu." 
+		},
+		'8' : {
+			'Ti' : "物事を複雑にする可能性が高くなります。monogoto wo fukuzatsu ni suru kanō sei ga takaku nari masu.",
+			'Te' : "物事を単純化する可能性が高くなります。monogoto wo tanjun ka suru kanō sei ga takaku nari masu." 
+		},
+		'9' : {
+			'Ti' : "規律に問題があると思われます。kiritsu ni mondai ga aru to omowa re masu.",
+			'Te' : "あなたは、何かを達成するためのあなたの努力において、非常に規律を持っている傾向があります。anata wa, nani ka wo tassei suru tame no anata no doryoku nioite, hijō ni kiritsu wo motte iru keikō ga ari masu." 
+		},
+		'10' : {
+			'Ti' : "何が起こったのかは重要ですが、それが再び起こるというわけではありませんnani ga okotta no ka wa jūyō desu ga, sore ga futatabi okoru toyuu wake de wa ari mase n",
+			'Te' : "私たちは、何が起こったのかに依存して意思決定を行う必要がありますwatashi tachi wa, nani ga okotta no ka ni izon shi te ishi kettei wo okonau hitsuyō ga ari masu" 
+		},
+	},
+	'F' : {
+		'1' : {
+			'Fi' : "自分の内部的な価値観で状況を絞り込み、自分の気持ちを確認します。 あなたは、非常に個人的または直感的な倫理システムを持っていて、他の人に説明するのが難しい場合があります。jibun no naibu teki na kachi kan de jōkyō wo shiborikomi, jibun no kimochi wo kakunin shi masu. anata wa, hijō ni kojin teki matawa chokkan teki na rinri shisutemu wo motte i te, ta no hito ni setsumei suru no ga muzukashii baai ga ari masu.",
+			'Fe' : "値は客観的で共有されます。 社会的規範や期待（エチケットなど）に依存して道徳性を求めます。ne wa kyakkan teki de kyōyū sa re masu. shakai teki kihan ya kitai (echiketto nado) ni izon shi te dōtoku sei wo motome masu." 
+		},
+		'2' : {
+			'Fi' : "真正性と誠実さはあなたにとって重要であり、他者が偽であるか、または誠実でないように見えることは嫌いです。 個人的な感情を共有するのは、より消極的です。shinsei sei to seijitsu sa wa anata nitotte jūyō de ari, tasha ga nise de aru ka, matawa seijitsu de nai yō ni mieru koto wa kirai desu. kojin teki na kanjō wo kyōyū suru no wa, yori shōkyoku teki desu.",
+			'Fe' : "他者の感情に責任を持ち、自分の行動を調整して、相手が快適に感じられるようにします。 あなたは自分自身に対して非常にオープンな傾向があります。tasha no kanjō ni sekinin wo mochi, jibun no kōdō wo chōsei shi te, aite ga kaiteki ni kanji rareru yō ni shi masu. anata wa jibun jishin nitaishite hijō ni ōpun na keikō ga ari masu." 
+		},
+		'3' : {
+			'Fi' : "グループが共有する感情的なアウトバーストは、感情に対する外部の感情的なサポートを必要としない、または必要としないため、不快になります。gurūpu ga kyōyū suru kanjō teki na auto bāsuto wa, kanjō nitaisuru gaibu no kanjō teki na sapōto wo hitsuyō to shi nai, matawa hitsuyō to shi nai tame, fukai ni nari masu.",
+			'Fe' : "対立する意見を交渉し、共通の地を見つけることで、グループの調和を維持しようとします。tairitsu suru iken wo kōshō shi, kyōtsū no chi wo mitsukeru koto de, gurūpu no chōwa wo iji shiyo u to shi masu." 
+		},
+		'4' : {
+			'Fi' : "状況を理解するには、他のすべての人に見られる客観的な事実よりも、自分がその状況をどのように感じているかを理解する必要があります。jōkyō wo rikai suru ni wa, ta no subete no hito ni mi rareru kyakkan teki na jijitsu yori mo, jibun ga sono jōkyō wo dono yō ni kanji te iru ka wo rikai suru hitsuyō ga ari masu.",
+			'Fe' : "行動のモラルな影響に注意し、物事がいかにであるかよりも、物事がどのようになるかについて関心を持っている。kōdō no moraru na eikyō ni chūi shi, monogoto ga ikani de aru ka yori mo, monogoto ga dono yō ni naru ka nitsuite kanshin wo motte iru." 
+		},
+		'5' : {
+			'Fi' : "あなたは自分が感じていることに耳を傾け、自分自身を表現し、体験やアートを通じて感情を共有しようとしますanata wa jibun ga kanji te iru koto ni mimi wo katamuke, jibun jishin wo hyōgen shi, taiken ya āto wotsūjite kanjō wo kyōyū shiyo u to shi masu",
+			'Fe' : "他者の感情に耳を傾け、そのニーズを満たすよう積極的に努力します。tasha no kanjō ni mimi wo katamuke, sono nīzu wo mitasu yō sekkyoku teki ni doryoku shi masu." 
+		},
+		'6' : {
+			'Fi' : "自己検索の状態が一定である傾向があります。 あなたは、自分の内面を理解し、理想的な人物になるように努力します。 その結果、自分の個人的な感情を自然に認識し、自分とつながりを持つことで他者との関係を築く傾向があります。jiko kensaku no jōtai ga ittei de aru keikō ga ari masu. anata wa, jibun no naimen wo rikai shi, risō teki na jinbutsu ni naru yō ni doryoku shi masu. sono kekka, jibun no kojin teki na kanjō wo shizen ni ninshiki shi, jibun to tsunagari wo motsu koto de tasha to no kankei wo kizuku keikō ga ari masu.",
+			'Fe' : "他者の感情的な反応に自然に焦点を当てる傾向があります。 あなたはこれらの反応に敏感であり、それに応じて行動を適応させる傾向があります。 これは誰か他の人のための端にあなた自身を押す傾向があるので、何人かの人々を戸口/心理学者としてあなたを参照するように導くかもしれない。tasha no kanjō teki na hannō ni shizen ni shōten wo ateru keikō ga ari masu. anata wa korera no hannō ni binkan de ari, sore ni ōji te kōdō wo tekiō sa seru keikō ga ari masu. kore wa dare ka ta no hito no tame no haji ni anata jishin wo osu keikō ga aru node, nan nin ka no hitobito wo toguchi/shinri gakusha toshite anata wo sanshō suru yō ni michibiku kamo shire nai." 
+		},
+		'7' : {
+			'Fi' : "自分の個人的な感情を他の人に投影しています。 これは、共感がないことを意味するわけではありませんjibun no kojin teki na kanjō wo ta no hito ni tōei shi te i masu. kore wa, kyōkan ga nai koto wo imi suru wake de wa ari mase n",
+			'Fe' : "自分の感情に関係なく、他者の行動を理解します。jibun no kanjō ni kankei naku, tasha no kōdō wo rikai shi masu." 
+		},
+		'8' : {
+			'Fi' : "自分の目標ではなくても、相手の感情を傷つけることがあります。この人々を相手にしたことがあるからです。jibun no mokuhyō de wa naku te mo, aite no kanjō wo kizutsukeru koto ga ari masu. kono hitobito wo aite ni shi ta koto ga aru kara desu.",
+			'Fe' : "相手のニーズや期待に基づいて、感情的に適切な方法で相手に対応することが自然に良い傾向があります。aite no nīzu ya kitai ni motozui te, kanjō teki ni tekisetsu na hōhō de aite ni taiō suru koto ga shizen ni yoi keikō ga ari masu." 
+		},
+		'9' : {
+			'Fi' : "あなたはあなたがどのように感じているかを知っています。anata wa anata ga dono yō ni kanji te iru ka wo shitte i masu.",
+			'Fe' : "感情と他者の感情をミックスします。kanjō to tasha no kanjō wo mikkusu shi masu." 
+		},
+		'10' : {
+			'Fi' : "他者の経験に共感できる場合は、非常に共感的です。（共感とは、他者の感情を自分自身で体験しているかのように理解する能力を表す用語です。 それは、自分の感情を何かに投影することも意味します）。tasha no keiken ni kyōkan dekiru baai wa, hijō ni kyōkan teki desu. (kyōkan to wa, tasha no kanjō wo jibun jishin de taiken shi te iru ka no yō ni rikai suru nōryoku wo arawasu yōgo desu. sore wa, jibun no kanjō wo nani ka ni tōei suru koto mo imi shi masu).",
+			'Fe' : "あなたはとても同情的です。 （同情とは、他人の感情を共有する能力であり、最も多くの場合、不幸のために痛みを感じる能力を指します）。anata wa totemo dōjō teki desu. (dōjō to wa, tanin no kanjō wo kyōyū suru nōryoku de ari, mottomo ōku no baai, fukō no tame ni itami wo kanjiru nōryoku wo sashi masu)." 
+		},
+	},
+	'S' : {
+		'1' : {
+			'Si' : "現在の状況を、過去に経験したことや、通常と考えていることと比較対照します。genzai no jōkyō wo, kako ni keiken shi ta koto ya, tsūjō to kangae te iru koto to hikaku taishō shi masu.",
+			'Se' : "現在、物理的な世界で起こっていることに注目しています。 五感を持っているほとんどの人よりも、あなたの方が知覚的です。genzai, butsuri teki na sekai de okotte iru koto ni chūmoku shi te i masu. gokan wo motte iru hotondo no hito yori mo, anata no hō ga chikaku teki desu." 
+		},
+		'2' : {
+			'Si' : "以前の知識と経験を基に、新しい情報を主観的にフィルタリングします。izen no chishiki to keiken wo moto ni, atarashii jōhō wo shukan teki ni firuta ringu shi masu.",
+			'Se' : "自分の判断なしで、個人的な偏見を持たず、隠された意味を見ずに、世界に何があるかを客観的に見ることができます。jibun no handan nashi de, kojin teki na henken wo mota zu, kakusa re ta imi wo mi zu ni, sekai ni nani ga aru ka wo kyakkan teki ni miru koto ga deki masu." 
+		},
+		'3' : {
+			'Si' : "あなたは過去を記憶し、保存するのが好きで、経験したことと本当のことを好むのです。 あなたが大事にする事はルーチンおよびよく知られていて、新しい事はあなたに時間をかけることができる。anata wa kako wo kioku shi, hozon suru no ga suki de, keiken shi ta koto to hontō no koto wo konomu no desu. anata ga daiji ni suru koto wa rūchin oyobi yoku shira re te i te, atarashii koto wa anata ni jikan wo kakeru koto ga dekiru.",
+			'Se' : "あなたのまわりで起こる具体的な事に続きなさい:イメージ、臭い、様式、傾向および何をするか。anata no mawari de okoru gutai teki na koto ni tsuzuki nasai:imēji, kusai, yōshiki, keikō oyobi nani wo suru ka." 
+		},
+		'4' : {
+			'Si' : "痛み、飢え、喉の渇きなど、体内の感覚に非常に気を配っています。itami, ue, nodo no kawaki nado, tainai no kankaku ni hijō ni ki wo kutte i masu.",
+			'Se' : "身体の美しさと新しい感覚が好きです。 スリルを求める人、ファッションの達人、または救助者である場合もある。shintai no utsukushi sa to atarashii kankaku ga suki desu. suriru wo motomeru hito, fasshon no tatsujin, matawa kyūjo sha de aru baai mo aru." 
+		},
+		'5' : {
+			'Si' : "新しい情報を吸収し、日常業務に注意を払いながら、過去の行動や経験と慎重に比較します。atarashii jōhō wo kyūshū shi, nichijō gyōmu ni chūi wo harai nagara, kako no kōdō ya keiken to shinchō ni hikaku shi masu.",
+			'Se' : "行動を起こすためには、外部からのブーストが必要になることがよくあります。kōdō wo okosu tame ni wa, gaibu kara no būsuto ga hitsuyō ni naru koto ga yoku ari masu." 
+		},
+		'6' : {
+			'Si' : "確認します。kakunin shi masu.",
+			'Se' : "あなたは反応しています。anata wa hannō shi te i masu." 
+		},
+		'7' : {
+			'Si' : "あなたは過去のミスから学びますanata wa kako no misu kara manabi masu",
+			'Se' : "過去の影響を忘れがちです。kako no eikyō wo wasure gachi desu." 
+		},
+		'8' : {
+			'Si' : "行動する前に考えてください。kōdō suru mae ni kangae te kudasai.",
+			'Se' : "考える前に行動します。kangaeru mae ni kōdō shi masu." 
+		},
+		'9' : {
+			'Si' : "長期的な満足を求めています。chōki teki na manzoku wo motome te i masu.",
+			'Se' : "短期的には満足感を得ることを好みます。 カルペディエムtanki teki ni wa manzoku kan wo eru koto wo konomi masu. karupediemu" 
+		},
+		'10' : {
+			'Si' : "長期的に安定した生活様式を維持することを可能にするより適度な人々である傾向がある。 例えば、買物をすることを楽しむことができるのにあなたのお金と注意する傾向がありなさい。chōki teki ni antei shi ta seikatsu yōshiki wo iji suru koto wo kanō ni suru yori tekido na hitobito de aru keikō ga aru. tatoeba, kaimono wo suru koto wo tanoshimu koto ga dekiru noni anata no okane to chūi suru keikō ga ari nasai.",
+			'Se' : "あなた自身を満足させ、衝動的に行動したいと思いなさい。 生命があらゆる時に完全に住まなければならないので何かがほしいと思うか、または物理的な刺激か喜びのあなた自身を奪い取るときあなた自身に「NO」を言うことは困難であることが分るかもしれない。 明らかに、より古いまたはより成熟すればより適度である。anata jishin wo manzoku sa se, shōdō teki ni kōdō shi tai to omoi nasai. seimei ga arayuru toki ni kanzen ni suma nakere ba nara nai node nani ka ga hoshii to omou ka, matawa butsuri teki na shigeki ka yorokobi no anata jishin wo ubaitoru toki anata jishin ni 'NO' wo iu koto wa konnan de aru koto ga wakaru kamo shire nai. akiraka ni, yori furui matawa yori seijuku sure ba yori tekido de aru." 
+		},
+	},
+	'N' : {
+		'1' : {
+			'Ni' : "いくつかの良いアイデアに焦点を当て、他の人を除外して、気が散ることを防ぎます。ikutsu ka no yoi aidea ni shōten wo ate, ta no hito wo jogai shi te, ki ga chiru koto wo fusegi masu.",
+			'Ne' : "あなたはまだ決まっていません。 ブレーンストーミングとジャグを一度に行うのが好きです。anata wa mada kimatte i mase n. burēnsutōmingu to jagu wo ichi do ni okonau no ga suki desu." 
+		},
+		'2' : {
+			'Ni' : "あなたは目標や夢に焦点を当て、未来を現実にするためのアイデアを求めています。anata wa mokuhyō ya yume ni shōten wo ate, mirai wo genjitsu ni suru tame no aidea wo motome te i masu.",
+			'Ne' : "新しい事実を外部から吸収し、新しいアイデアを生み出す想像力豊かな方法で点を結び付けます。atarashii jijitsu wo gaibu kara kyūshū shi, atarashii aidea wo umidasu sōzō ryoku yutaka na hōhō de ten wo musubitsuke masu." 
+		},
+		'3' : {
+			'Ni' : "その代り、それはあなたの頭脳が無意識に処置をしているように感じ、あなたが行動したいと思うようにするトリガーの時に突然達する傾向がありなさい。sono kawari, sore wa anata no zunō ga muishiki ni shochi wo shi te iru yō ni kanji, anata ga kōdō shi tai to omou yō ni suru torigā no toki ni totsuzen tassuru keikō ga ari nasai.",
+			'Ne' : "あなたの世界は考えの爆発であり、意識的に構成要素に事を分解し、単語の後ろの考えの意味を見、他の人がしない関係を作る。anata no sekai wa kangae no bakuhatsu de ari, ishiki teki ni kōsei yōso ni koto wo bunkai shi, tango no ushiro no kangae no imi wo mi, ta no hito ga shi nai kankei wo tsukuru." 
+		},
+		'4' : {
+			'Ni' : "より多くのトピックを掘り下げ、情報を組み合わせて、一貫性のある単一のアイデアを思い付くことができます。yori ōku no topikku wo horisage, jōhō wo kumiawase te, ikkan sei no aru tanitsu no aidea wo omoitsuku koto ga deki masu.",
+			'Ne' : "さまざまな趣味があり、前後に弾むことを楽しんでいます。 あなたは、可能性を探ることに集中しています。samazama na shumi ga ari, zengo ni hazumu koto wo tanoshin de i masu. anata wa, kanō sei wo saguru koto ni shūchū shi te i masu." 
+		},
+		'5' : {
+			'Ni' : "仕事を開始できるように選択したいと考えています。 さまざまな角度から状況を確認し、内部の世界への影響に注意します。shigoto wo kaishi dekiru yō ni sentaku shi tai to kangae te i masu. samazama na kakudo kara jōkyō wo kakunin shi, naibu no sekai e no eikyō ni chūi shi masu.",
+			'Ne' : "あなたはオープンな気がしています。 刺激的な精神的インスピレーションがない状況は嫌いです。anata wa ōpun na ki ga shi te i masu. shigeki teki na seishin teki insupirēshon ga nai jōkyō wa kirai desu." 
+		},
+		'6' : {
+			'Ni' : "あなたは自分の考えを自分自身に伝えます。anata wa jibun no kangae wo jibun jishin ni tsutae masu.",
+			'Ne' : "あなたは、あなたの考えや想像力を共有したいと思っています。anata wa, anata no kangae ya sōzō ryoku wo kyōyū shi tai to otte i masu." 
+		},
+		'7' : {
+			'Ni' : "あなたはほとんど知っていません。anata wa hotondo shitte i mase n.",
+			'Ne' : "多くのことを少し知っています。ōku no koto wo sukoshi shitte i masu." 
+		},
+		'8' : {
+			'Ni' : "自然な結論を出す傾向があるか、1つの可能性だけを見る傾向があります。 基本的には結論にジャンプしているか、非常に具体的な方法で周囲の情報を解釈しているとします。shizen na ketsuron wo dasu keikō ga aru ka, 1 tsu no kanō sei dake wo miru keikō ga ari masu. kihon teki ni wa ketsuron ni janpu shi te iru ka, hijō ni gutai teki na hōhō de shūi no jōhō wo kaishaku shi te iru to shi masu.",
+			'Ne' : "あなたはアイデアを持っている人であり、ブレインストーミングを行っています。 あなたは「What if ...」の人です。 世界はあらゆる可能性を秘めています。つまり、世界を選択するのが困難な場合があり、その結果、何かを完全に関与させることができない状態になります。anata wa aidea wo motte iru hito de ari, bureinsutōmingu wo otte i masu. anata wa 'What if ...' no hito desu. sekai wa arayuru kanō sei wo hime te i masu. tsumari, sekai wo sentaku suru no ga konnan na baai ga ari, sono kekka, nani ka wo kanzen ni kanyo sa seru koto ga deki nai jōtai ni nari masu." 
+		},
+		'9' : {
+			'Ni' : "あなたは、何が協調であるか、何が好きです。 抽象化は、ユーザーが楽しんでいるものではありません。anata wa, nani ga kyōchō de aru ka, nani ga suki desu. chūshō ka wa, yūzā ga tanoshin de iru mono de wa ari mase n.",
+			'Ne' : "知的抽象化が好きです。chiteki chūshō ka ga suki desu." 
+		},
+		'10' : {
+			'Ni' : "1つのことに集中する傾向があります。 絶対的に関連性のあるものだけを共有したいと思います。 理由を簡単に追跡できます1 tsu no koto ni shūchū suru keikō ga ari masu. zettai teki ni kanren sei no aru mono dake wo kyōyū shi tai to omoi masu. riyū wo kantan ni tsuiseki deki masu",
+			'Ne' : "あなたとの会話はすぐに広がります。 短期間でさまざまなトピックを取り上げる傾向があります。 奇妙な、または折衷的なようであるが、追跡可能でなければならない主題間で跳ぶ。anata to no kaiwa wa sugu ni hirogari masu. tankikan de samazama na topikku wo toriageru keikō ga ari masu. kimyō na, matawa secchū teki na yō de aru ga, tsuiseki kanō de nakere ba nara nai shudai kan de tobu." 
+		},
+	}
+}
