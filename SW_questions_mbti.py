a36c6b561984e2ec2eba8266a776c2ee386cea35
aderepas <49250442+aderepas@users.noreply.github.com>
2022-03-29 22:58:54 +0200

    Add files via upload

---
+++ SW_questions_mbti.py
@@ -0,0 +1,170 @@
+question_about_mbti = {
+	'T' : {
+		'1' : {
+			'Ti' : "Du gillar att starta nya projekt. Du är som arkitekt, du bygger din lösning genom att utforska alla möjligheter att hitta den bästa metoden.",
+			'Te' : "Du gillar att avsluta projekt. Det är bättre att vara klar än att hitta den bästa lösningen på 100%. I händelse av en kris, är du befälhavaren som driver laget att avsluta." 
+		},
+		'2' : {
+			'Ti' : "Du sorterar idéerna i ditt sinne för att utveckla dem senare, och du gillar att förstå hur saker och ting fungerar.",
+			'Te' : "Du är bra på schemaläggning, kvantifiering och beredskapsplanering." 
+		},
+		'3' : {
+			'Ti' : "Du gillar att identifiera hur saker och ting är olika och rangordna dem logiskt.",
+			'Te' : "Du vill ha kontroll och du gillar inte människor som försöker påverka dig." 
+		},
+		'4' : {
+			'Ti' : "Du brinner för kunskap för sig själv. Noggrannhet är viktigt, och oavsett var en idé kommer ifrån om det är bäst.",
+			'Te' : "De bästa idéerna är effektiva och omedelbart användbara. Informationen måste vara objektiv, ordnad och rationell." 
+		},
+		'5' : {
+			'Ti' : "Du är glad att följa varje steg i vägen till vår slutsats. Betydelsen är viktigare än rena fakta, och du vill förstå vad som händer bakom kulisserna.",
+			'Te' : "Du vill bara ha något som fungerar effektivt. Ni är mer bekymrade över slutresultatet än med förståelse för alla stadier och processer av reflektion som ledde till det. Fakta är perfekta, det finns inget behov av att dra bredare slutsatser." 
+		},
+		'6' : {
+			'Ti' : "För dig är det viktigare att förstå ett koncept än dess tillämpningar.",
+			'Te' : "För er är effektiviteten hos en lösning viktigare än hur saker och ting hände." 
+		},
+		'7' : {
+			'Ti' : "Du är deduktiv i naturen. Du utvecklar teorier och sedan flyttar du till fakta. Du gör antaganden baserat på vad som är vettigt för dig.",
+			'Te' : "Du är induktiv i naturen. Du bestämmer vad som är sakligt och sedan utvecklar du teorier baserade på det. Du fattar beslut utifrån objektiva fakta som presenteras för dig utifrån..." 
+		},
+		'8' : {
+			'Ti' : "Det är mer troligt att du kommer att komplicera saker och ting.",
+			'Te' : "Det är mer troligt att du förenklar saker och ting." 
+		},
+		'9' : {
+			'Ti' : "Du har förmodligen problem med disciplin.",
+			'Te' : "Du tenderar att vara mycket disciplinerad i dina ansträngningar för att uppnå något." 
+		},
+		'10' : {
+			'Ti' : "Det som hände är viktigt, men det betyder inte att det kommer att hända igen",
+			'Te' : "Vi måste förlita oss på vad som hände med att fatta beslut" 
+		},
+	},
+	'F' : {
+		'1' : {
+			'Fi' : "Du filtrerar en situation genom dina egna interna värden för att se hur du mår. Du har ett mycket personligt eller gutbaserat etiksystem som kan vara svårt att förklara för andra.",
+			'Fe' : "Värden är objektiva och delade. Du förlitar dig på sociala normer och förväntningar (såsom etikett) för moral." 
+		},
+		'2' : {
+			'Fi' : "Äkthet och uppriktighet är viktigt för dig, och du hatar att andra verkar falska eller insincere. Du är mer motvillig att dela med dig av dina personliga känslor.",
+			'Fe' : "Du känner dig ansvarig för andras känslor och anpassar ditt beteende för att få dem att känna sig bekväma. Du tenderar att vara mycket öppen för dig själv." 
+		},
+		'3' : {
+			'Fi' : "Känslomässiga utbrott som delas av en grupp gör dig obekväm eftersom du inte behöver eller vill ha externt känslomässigt stöd för dina känslor.",
+			'Fe' : "Ni försöker upprätthålla gruppens harmoni genom att förhandla fram motstridiga åsikter och finna gemensamma grunder." 
+		},
+		'4' : {
+			'Fi' : "Du tolkar situationer mer genom hur du känner dig om dem än genom objektiva fakta som är synliga för alla andra.",
+			'Fe' : "Du uppmärksammar de moraliska konsekvenserna av handlingar och du bryr dig mer om hur saker och ting ska vara än hur de är." 
+		},
+		'5' : {
+			'Fi' : "Du lyssnar på vad du känner och försöker uttrycka dig själv och dela dina känslor genom upplevelser eller konst.",
+			'Fe' : "Du lyssnar på andras känslor och försöker aktivt uppfylla deras behov." 
+		},
+		'6' : {
+			'Fi' : "Du tenderar att vara i ett konstant tillstånd av självsökning. Du strävar efter att förstå ditt inre jag och forma dig själv till den ideala personen. Som ett resultat tenderar du att vara naturligt medveten om dina personliga känslor och bygga relationer med andra genom att ansluta med dig själv.",
+			'Fe' : "Du tenderar att fokusera naturligt på andras känslomässiga reaktioner. Du är känslig för dessa reaktioner och tenderar att anpassa deras beteende därefter. Detta kan leda till att vissa människor hänvisar till dig som dörrmatta/psykolog, eftersom du tenderar att driva dig över kanten för någon annan." 
+		},
+		'7' : {
+			'Fi' : "Du projicerar dina personliga känslor på någon annan. Det betyder inte att du inte har empati",
+			'Fe' : "Du förstår andras handlingar oavsett dina känslor." 
+		},
+		'8' : {
+			'Fi' : "Du kan ibland skada andras känslor även om det inte är ditt mål, eftersom du har satt på detta folk, du känslor.",
+			'Fe' : "Du tenderar att vara naturligt bra på att svara på andra på ett känslomässigt lämpligt sätt, baserat på den berörda personens behov och förväntningar." 
+		},
+		'9' : {
+			'Fi' : "Du vet hur du känner.",
+			'Fe' : "Du blandar känslor med andras känslor." 
+		},
+		'10' : {
+			'Fi' : "Du är mycket empatisk när du kan relatera till någon annans upplevelse. (Empati är en term som används för att beskriva förmågan att förstå andra människors känslor som om vi själva upplevde dem. Det kan också innebära att projicera våra egna känslor på något.)",
+			'Fe' : "Du är mycket sympatisk. (Sympati avser förmågan att dela känslor av en annan person, oftast känner smärta för deras olycka.)" 
+		},
+	},
+	'S' : {
+		'1' : {
+			'Si' : "Du jämför och kontrasterar din nuvarande situation med vad du har upplevt tidigare och vad du anser vara normalt.",
+			'Se' : "Du är uppmärksam på vad som händer runt omkring dig i den fysiska världen just nu. Du är mer perceptiv än de flesta med dina fem sinnen." 
+		},
+		'2' : {
+			'Si' : "Du filtrerar subjektivt ny information genom dina tidigare kunskaper och erfarenheter.",
+			'Se' : "Du ser objektivt vad som finns i världen omkring dig utan dom, utan personlig fördom och utan att se någon dold mening." 
+		},
+		'3' : {
+			'Si' : "Du älskar att komma ihåg och bevara det förflutna, och du föredrar vad som är upplevt och sant. De saker du värdesätter är rutinmässiga och bekanta, och nya saker kan ta tid att behaga dig.",
+			'Se' : "Du följer de konkreta saker som händer runt omkring dig: Bilder, dofter, stilar, trender och vad folk omkring dig gör." 
+		},
+		'4' : {
+			'Si' : "Du är mycket uppmärksam på dina inre kroppsliga känslor, såsom smärta, hunger och törst.",
+			'Se' : "Du älskar fysisk skönhet och nya känslor. Du kan vara en spänningssökare, en modeguru eller en räddare." 
+		},
+		'5' : {
+			'Si' : "Du assimilerar ny information och jämför den noggrant med ditt tidigare beteende och erfarenheter, samtidigt som du uppmärksammar din rutin.",
+			'Se' : "Du behöver ofta ett uppsving från omvärlden för att få dig att agera." 
+		},
+		'6' : {
+			'Si' : "Du observerar.",
+			'Se' : "Du reagerar." 
+		},
+		'7' : {
+			'Si' : "Du lär dig av dina tidigare misstag",
+			'Se' : "Du tenderar att glömma effekterna av det förflutna." 
+		},
+		'8' : {
+			'Si' : "Du tänker framför dig agerar.",
+			'Se' : "Du agerar innan du tänker." 
+		},
+		'9' : {
+			'Si' : "Du vill ha långsiktig tillfredsställelse.",
+			'Se' : "Du föredrar att leva en tillfredsställelse på kort sikt. Carpe diem." 
+		},
+		'10' : {
+			'Si' : "Du tenderar att vara mer moderat människor, vilket gör att du kan upprätthålla en stabil livsstil på lång sikt. Du tenderar att vara försiktig med dina pengar, även om du till exempel kan njuta av shopping.",
+			'Se' : "Du vill skämma bort dig själv och agera impulsivt. Du kan ha svårt att säga NEJ till dig själv när du vill ha något, eller att beröva dig fysisk stimulering eller njutning, eftersom livet måste leva till fullo vid varje tillfälle. Självklart, om du är äldre eller mer mogen du är mer måttlig." 
+		},
+	},
+	'N' : {
+		'1' : {
+			'Ni' : "Du fokuserar på några bra idéer och utesluter andra för att undvika distraktioner.",
+			'Ne' : "Du är obestämt. Du gillar brainstorming och juggling av mycket tankar genast i ditt sinne." 
+		},
+		'2' : {
+			'Ni' : "Du fokuserar på dina mål eller drömmar och letar efter idéer för att göra din framtid till verklighet.",
+			'Ne' : "Du absorberar nya fakta från omvärlden och kopplar ihop punkterna på fantasifulla sätt för att skapa nya idéer." 
+		},
+		'3' : {
+			'Ni' : "Istället känns det som om din hjärna gör behandlingen omedvetet, och du tenderar att plötsligt nå triggningsmoment som får dig att vilja agera.",
+			'Ne' : "Er värld är en explosion av idéer, ni dekonstruerar medvetet saker i sina beståndsdelar, ni ser innebörden av idéer bakom ord, och ni gör kopplingar som andra inte gör." 
+		},
+		'4' : {
+			'Ni' : "Du fördjupar dig i färre ämnen och kombinerar bitar av information för att komma fram till en enda, sammanhängande idé.",
+			'Ne' : "Du har en mängd olika hobbyer och njuta studsar fram och tillbaka. Du fokuserar på att utforska möjligheter." 
+		},
+		'5' : {
+			'Ni' : "Du gillar att göra ett val så att du kan få arbeta. Du ser situationer från många håll och uppmärksamma påverkan på din inre värld.",
+			'Ne' : "Du är öppen. Du hatar situationer som saknar spännande mental inspiration." 
+		},
+		'6' : {
+			'Ni' : "Du håller dina tankar till dig själv.",
+			'Ne' : "Du gillar att dela med dig av dina tankar, din fantasi." 
+		},
+		'7' : {
+			'Ni' : "Du vet mycket om mycket lite.",
+			'Ne' : "Du vet lite om en hel del saker." 
+		},
+		'8' : {
+			'Ni' : "Du tenderar att dra en naturlig slutsats, eller se bara en möjlighet. Du hoppar i huvudsak till en slutsats, eller du tolkar informationen omkring dig på ett mycket specifikt sätt.",
+			'Ne' : "Du är de som har idéer, som brainstorma. Du är ”vad om...” människor. Du ser världen i alla dess möjligheter, vilket innebär att du kan ha svårt att välja en, vilket resulterar i ett tillstånd av obeslutsamhet eller oförmåga att fullt ut engagera sig i något." 
+		},
+		'9' : {
+			'Ni' : "Du gillar vad som är konserter, vad är. Abstraktion är inte något du tycker om.",
+			'Ne' : "Du gillar intellektuell abstraktion." 
+		},
+		'10' : {
+			'Ni' : "Du kommer att fokusera på en sak. Du kommer bara att vilja dela det som är absolut relevant. Vi kan enkelt följa ditt resonemang",
+			'Ne' : "Konversationer med dig kan fort utbredning ut. Du tenderar att täcka en mängd olika ämnen på kort tid. Du hoppar mellan ämnen, som kan verka udda eller eklektiska, men som måste vara spårbara." 
+		},
+	}
+}
